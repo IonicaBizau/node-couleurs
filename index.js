37584cff358ed1af47216df9b2244b0b24252ef6
Ionică Bizău <bizauionica@yahoo.com>
2014-04-14 12:00:26 +0300

    Removed new line and added comments

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+// dependencies
 var x256 = require("x256")
 
+// add rgb method to the string prototype
 String.prototype.rgb = function (r, g, b) {
-    return "\x1b[38;5;" + x256 (r, g, b) + "m" + this + "\033[0m\n";
+    return "\x1b[38;5;" + x256 (r, g, b) + "m" + this + "\033[0m";
 };
