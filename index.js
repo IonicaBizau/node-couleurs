4f5ea6a016977202634ebeffcdd903000433f0e2
Ionică Bizău <bizauionica@yahoo.com>
2014-08-11 11:58:39 +0300

    Added bold, italic etc styles

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
-// dependencies
+// Dependencies
 var x256 = require("x256")
 
-// add rgb method to the string prototype
-String.prototype.rgb = function rgb (r, g, b) {
+/**
+ * rgb
+ * Creates a colored string providing the color.
+ *
+ * @name rgb
+ * @function
+ * @param {String|Array|Number} r If number, it will be the red value from RGB.
+ * If array, it should be an array of three numbers representing RGB values.
+ * If String, it will be interpreted as HEX color.
+ * @param {Number} g Green value
+ * @param {Number} b Blue value
+ * @return {String} Colored string
+ */
+String.prototype.rgb = function rgb(r, g, b) {
 
     if (r[0] === "#") {
         return rgb.apply(this, hexToRgb(r));
     }
 
-    return "\x1b[38;5;" + x256 (r, g, b) + "m" + this + "\033[0m";
+    return "\x1b[38;5;" + x256(r, g, b) + "m" + this + "\033[0m";
 };
+
+
+var map = {
+    bold: ["\x1B[1m", "\x1B[22m"],
+    italic: ["\x1B[3m", "\x1B[23m"],
+    underline: ["\x1B[4m", "\x1B[24m"],
+    inverse: ["\x1B[7m", "\x1B[27m"],
+    strikethrough: ["\x1B[9m", "\x1B[29m"],
+};
+
+for (var key in map) {
+    (function (style, styleId) {
+        String.prototype[styleId] = function () {
+            return style[0] + this + style[1];
+        };
+    })(map[key], key)
+}
 
 /**
  * hexToRgb
