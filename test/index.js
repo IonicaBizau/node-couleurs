a4cbf62fa68a65d394b716de3c23afd4dfd6381a
Ionică Bizău <bizauionica@yahoo.com>
2014-08-11 12:13:58 +0300

    Updated test

--- test/index.js
+++ test/index.js
@@ -1,3 +1,20 @@
 var Couleurs = require ("../index");
 
-console.log("Hi!".rgb(255, 255, 0));
+console.log("Red".rgb([255, 0, 0]));
+console.log("Yellow".rgb(255, 255, 0));
+console.log("Blue".rgb("#2980b9"));
+
+console.log("Bold".bold())
+console.log("Italic".italic())
+console.log("Underline".underline())
+console.log("Inverse".inverse())
+console.log("Strikethrough".strikethrough())
+
+console.log("All combined"
+    .rgb("#d35400")
+    .bold()
+    .italic()
+    .underline()
+    .inverse()
+    .strikethrough()
+)
