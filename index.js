a4a0200dc6e05bf3cabfbc8779438a114b613736
Ionică Bizău <bizauionica@yahoo.com>
2014-11-15 20:12:55 +0200

    JSDoc comments and fixes.

--- index.js
+++ index.js
@@ -10,6 +10,45 @@ const map = {
   , strikethrough: ["\x1B[9m", "\x1B[29m"]
 };
 
+/**!
+ * hexToRgb
+ * Converts a hex color code to rgb
+ *
+ * @name hexToRgb
+ * @function
+ * @param {String} hex The hex color value.
+ * @return {Array|null} An array containing `r`, `g`, `b` values. If the input is invalid, `null` will be returned.
+ */
+function hexToRgb (hex) {
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? [
+        parseInt(result[1], 16),
+        parseInt(result[2], 16),
+        parseInt(result[3], 16)
+    ] : null;
+}
+
+/**
+ * Couleurs
+ *
+ * @name Couleurs
+ * @function
+ * @param {Boolean|undefined} setStringProto If `true`, the prototype of String class will be modified.
+ * @return {Object} An object containing the following methods:
+ *
+ *  - `rgb`
+ *  - `bold`
+ *  - `italic`
+ *  - `underline`
+ *  - `inverse`
+ *  - `strikethrough`
+ *
+ */
 module.exports = function (setStringProto) {
 
     /**
@@ -25,53 +64,34 @@ module.exports = function (setStringProto) {
      * @param {Number} b Blue value
      * @return {String} Colored string
      */
-    function rgb(r, g, b) {
+    function rgb(str, r, g, b) {
         if (r[0] === "#") {
-            return rgb.apply(this, hexToRgb(r));
+            return rgb.call(this, str, hexToRgb(r));
         }
 
-        return "\x1b[38;5;" + x256(r, g, b) + "m" + this + "\033[0m";
+        return "\x1b[38;5;" + x256(r, g, b) + "m" + str + "\033[0m";
     }
 
+    // Build couleurs object
     var couleurs = { rgb: rgb };
     for (var key in map) {
         (function (style, styleId) {
             couleurs[styleId] = function (str) {
-                if (!str && typeof this === "string") {
-                    str = this;
-                }
                 return style[0] + str + style[1];
             };
         })(map[key], key)
     }
 
-    if (setStringProto) {
+    // Modify String prototype
+    if (setStringProto === true) {
         for (var meth in couleurs) {
             (function (meth, func) {
-                String.prototype[meth] = func;
+                String.prototype[meth] = function (r, g, b) {
+                    return func.call(this, String(this), r, g, b);
+                };
             })(meth, couleurs[meth]);
         }
     }
 
     return couleurs;
 };
-
-/**
- * hexToRgb
- * Converts a hex color code to rgb
- *
- * @param hex
- */
-function hexToRgb (hex) {
-    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-        return r + r + g + g + b + b;
-    });
-
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? [
-        parseInt(result[1], 16),
-        parseInt(result[2], 16),
-        parseInt(result[3], 16)
-    ] : null;
-}
