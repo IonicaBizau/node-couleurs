71358903dc317e1b7642d849154e43fc775b060f
Ionică Bizău <bizauionica@yahoo.com>
2014-12-06 20:25:01 +0200

    Replaced rgb with fg

--- test/index.js
+++ test/index.js
@@ -2,9 +2,10 @@
 var Couleurs = require("../index")();
 
 // No prototype modify
-console.log(Couleurs.rgb("Red", [255, 0, 0]));
-console.log(Couleurs.rgb("Yellow", 255, 255, 0));
-console.log(Couleurs.rgb("Blue", "#2980b9"));
+console.log(Couleurs.fg("Red", [255, 0, 0]));
+console.log(Couleurs.fg("Yellow", 255, 255, 0));
+console.log(Couleurs.fg("Blue", "#2980b9"));
+console.log(Couleurs.bg("Blue Background", "#2980b9"));
 
 console.log(Couleurs.bold("Bold"));
 console.log(Couleurs.italic("Italic"));
@@ -17,7 +18,7 @@ console.log("Inverse".inverse());
 console.log("Strikethrough".strikethrough());
 
 console.log("All combined"
-    .rgb("#d35400")
+    .fg("#d35400")
     .bold()
     .italic()
     .underline()
