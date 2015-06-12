8c1c8d6ec9c638c1815988b3cf8cfa1c749e708c
Ionică Bizău <bizauionica@yahoo.com>
2015-06-12 11:02:37 +0300

    Added examples

---
+++ example/index.js
@@ -0,0 +1,28 @@
+// Dependencies
+var Couleurs = require("../lib");
+
+// Basic usage
+console.log(Couleurs.fg("Red", [255, 0, 0]));
+console.log(Couleurs.fg("Yellow", 255, 255, 0));
+console.log(Couleurs.fg("Blue", "#2980b9"));
+console.log(Couleurs.bg("Blue Background", "#2980b9"));
+console.log(Couleurs("Blue & Underline").fg("#2980b9").bold().toString());
+
+console.log(Couleurs.bold("Bold"));
+console.log(Couleurs.italic("Italic"));
+
+// Modify prototype
+Couleurs.proto();
+
+console.log("Underline".underline());
+console.log("Inverse".inverse());
+console.log("Strikethrough".strikethrough());
+
+console.log("All combined"
+    .fg("#d35400")
+    .bold()
+    .italic()
+    .underline()
+    .inverse()
+    .strikethrough()
+);
