e567f6923f778423062ba7469118effcd31df0ae
Ionică Bizău <bizauionica@yahoo.com>
2015-06-12 11:54:49 +0300

    Handle the foreground color

--- example/index.js
+++ example/index.js
@@ -3,6 +3,7 @@ var Couleurs = require("../lib");
 
 // Basic usage
 console.log(Couleurs.fg("Red", [255, 0, 0]));
+console.log(Couleurs("Red foreground", [255, 0, 0]));
 console.log(Couleurs.fg("Yellow", 255, 255, 0));
 console.log(Couleurs.fg("Blue", "#2980b9"));
 console.log(Couleurs.bg("Blue Background", "#2980b9"));
