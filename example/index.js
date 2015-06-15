2cd4a4f2899dc1e0610fe8018e3c1db3c4d99f52
Ionică Bizău <bizauionica@yahoo.com>
2015-06-15 21:34:25 +0300

    Fixed the style

--- example/index.js
+++ example/index.js
@@ -13,7 +13,7 @@ console.log(Couleurs("Red foreground", [255, 0, 0]));
 console.log(Couleurs.fg("Yellow", 255, 255, 0));
 console.log(Couleurs.fg("Blue", "#2980b9"));
 console.log(Couleurs.bg("Blue Background", "#2980b9"));
-console.log(Couleurs("Blue & Underline").fg("#2980b9").bold().toString());
+console.log(Couleurs("Blue & Underline").fg("#2980b9").bold().underline().toString());
 
 console.log(Couleurs.bold("Bold"));
 console.log(Couleurs.italic("Italic"));
