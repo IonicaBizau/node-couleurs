425f2e0b1755de4bfe6dc418b96a9c31866a2445
Ionică Bizău <bizauionica@gmail.com>
2016-03-02 12:13:44 +0200

    :rocket:

--- test/index.js
+++ test/index.js
@@ -1,36 +1,34 @@
+"use strict";
+
 // Dependencies
 var Couleurs = require("../lib")
-  , Assert = require("assert")
   , FlatColors = require("flat-colors")
+  , tester = require("tester")
   ;
 
-// Default behavior
-it("should support basic color support", function (cb) {
-    Assert.equal(
-        new Couleurs("Hello World")
-            .fg(FlatColors.colors[0])
-            .bg(FlatColors.colors[1])
-            .bold()
-            .toString()
-      , "\u001b[1m\u001b[48;5;41m\u001b[38;5;37mHello World\u001b[39m\u001b[49m\u001b[22m"
-    );
-    cb();
+tester.describe("couleurs", test => {
+    // Default behavior
+    test.should("support basic color support", () => {
+        let c = new Couleurs("Hello World")
+                .fg(FlatColors.colors[0])
+                .bg(FlatColors.colors[1])
+                .bold()
+                .toString();
+
+        test.expect(c).toBe("\u001b[1m\u001b[48;5;78m\u001b[38;5;79mHello World\u001b[39m\u001b[49m");
+    });
+
+    // Foreground color in constructor
+    test.should("support foreground color in constructor", () => {
+        var c = Couleurs("Hello World", FlatColors.colors[0]);
+        test.expect(c).toBe("\u001b[38;5;79mHello World\u001b[39m");
+    });
+
+    // Proto
+    test.should("handle prototype calls", () => {
+        Couleurs.proto();
+        var c = "Hello World".fg(FlatColors.colors[0]);
+        test.expect(c).toBe("\u001b[38;5;79mHello World\u001b[39m");
+    });
 });
 
-// Foreground color in constructor
-it("should support foreground color in constructor", function (cb) {
-    Assert.equal(
-        Couleurs("Hello World", FlatColors.colors[0])
-      , "\u001b[38;5;37mHello World\u001b[39m"
-    );
-    cb();
-});
-
-// Proto
-it("it should handle prototype calls", function (cb) {
-    Couleurs.proto();
-    Assert.equal("Hello World".fg(FlatColors.colors[0])
-      , "\u001b[38;5;37mHello World\u001b[39m"
-    );
-    cb();
-});
